68146b976d8eabc92761b62516db5ddb854064e5
陈雄 <478815108@qq.com>
2017-12-14 14:59:53 +0800

    new View

--- Example/Source/tool/TokenHybridOrganizer.h
+++ Example/Source/tool/TokenHybridOrganizer.h
@@ -7,12 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
-@class TokenNodeComponentRegister,TokenViewBuilder,TokenHybridRenderController;
+@class TokenNodeComponentRegister,TokenViewBuilder,TokenHybridRenderController,TokenHybridRenderView;
 @import UIKit;
 @interface TokenHybridOrganizer : NSObject
 @property(nonatomic ,strong) TokenNodeComponentRegister  *nodeComponentRegister;
 @property(nonatomic ,weak  ) TokenViewBuilder            *currentViewBuilder;
 @property(nonatomic ,weak  ) TokenHybridRenderController *currentViewController;
+@property(nonatomic ,weak  ) TokenHybridRenderView       *currentView;
+@property(nonatomic ,assign) BOOL                        renderInView;
 +(TokenHybridOrganizer *)sharedOrganizer;
 -(void)addPageDefaultWithSuiteName:(NSString *)name;
 -(void)clearAllPageDefaultsData;
