83eceaeffd392c9f3cb5e8f623b479a81e067ede
陈雄 <478815108@qq.com>
2017-12-19 15:47:51 +0800

    add new model

---
+++ Source/model/TokenAssociateContext.h
@@ -0,0 +1,16 @@
+//
+//  TokenAssociateContext.h
+//  TokenHybrid
+//
+//  Created by 陈雄 on 2017/12/19.
+//  Copyright © 2017年 com.feelings. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class TokenHybridRenderController,TokenHybridRenderView,TokenJSContext,TokenViewBuilder;
+@interface TokenAssociateContext : NSObject
+@property(nonatomic ,weak) TokenHybridRenderController *currentAssociateController;
+@property(nonatomic ,weak) TokenHybridRenderView       *currentAssociateView;
+@property(nonatomic ,weak) TokenViewBuilder            *currentAssociateViewBuilder;
+@end
---
+++ Source/model/TokenAssociateContext.m
@@ -0,0 +1,13 @@
+//
+//  TokenAssociateContext.m
+//  TokenHybrid
+//
+//  Created by 陈雄 on 2017/12/19.
+//  Copyright © 2017年 com.feelings. All rights reserved.
+//
+
+#import "TokenAssociateContext.h"
+
+@implementation TokenAssociateContext
+
+@end
