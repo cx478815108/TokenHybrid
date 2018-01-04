457ef44078847240a48735050a794771fbf9fc9c
陈雄 <478815108@qq.com>
2018-01-04 14:40:10 +0800

    new examples

--- Example/TokenHybrid/ViewController.m
+++ Example/TokenHybrid/ViewController.m
@@ -8,9 +8,10 @@
 
 #import "ViewController.h"
 #import "TokenHybridRenderController.h"
+#import "TokenHybridOrganizer.h"
+#import "CustomViewController.h"
 #import "UIColor+Token.h"
 
-@import JavaScriptCore;
 @interface ViewController ()
 @end
 
@@ -24,16 +25,26 @@
     [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName:UIColor.whiteColor}];
     [self.navigationController.navigationBar setTintColor:UIColor.whiteColor];
     [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
+    
 }
 
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
 }
 
-- (IBAction)button1Pressed:(id)sender {
-    NSString *url = @"https://raw.githubusercontent.com/cx478815108/TokenHybridHTML/master/examsEnter/examples.html";
-    TokenHybridRenderController *obj = [[TokenHybridRenderController alloc] initWithHTMLURL:url];
-    [self.navigationController pushViewController:obj animated:YES];
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.row == 0) {
+        NSString *url = @"https://raw.githubusercontent.com/cx478815108/TokenHybridHTML/master/examsEnter/examples.html";
+        TokenHybridRenderController *obj = [[TokenHybridRenderController alloc] initWithHTMLURL:url];
+        [self.navigationController pushViewController:obj animated:YES];
+    }
+    else if (indexPath.row == 1) {
+        CustomViewController *obj = [[CustomViewController alloc] init];
+        [self.navigationController pushViewController:obj animated:YES];
+    }
+    else if (indexPath.row == 2) {
+        [[TokenHybridOrganizer sharedOrganizer] clearAllPageDefaultsData];
+    }
 }
 
 @end
